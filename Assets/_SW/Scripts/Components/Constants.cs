722304608b0f3083b5ed5f8fc9a36eb81a9ed623
Alexey Zhardzetski <unitydev17@gmail.clom>
2023-08-27 06:51:00 +0300

    Replace markers classes to enums

--- Assets/_SW/Scripts/Components/Constants.cs
+++ Assets/_SW/Scripts/Components/Constants.cs
@@ -4,4 +4,7 @@ public static class Constants
     public const string WaterFlushAnimation = "WaterFlush";
     public const string FireIdleAnimation = "FireIdle";
     public const string FireFlushAnimation = "FireFlush";
+    
+    public const string Exception_TypeNotMatch = "Can not match TileType and BaseComponent.Type";
+    
 }
--- Assets/_SW/Scripts/Model/FireModel.cs
+++
@@ -1,3 +0,0 @@
-public class FireModel : ItemModel
-{
-}
